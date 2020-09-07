b9ee45e072f2e1f742f30375ca0fe7f7fcbb8fa9
Sadamune <zwiebelshi@gmail.com>
2020-09-07 11:41:57 +0900

    no message

--- LoginSystem/src/jdbc/UserInf.java
+++ LoginSystem/src/jdbc/UserInf.java
@@ -35,47 +35,47 @@ public class UserInf {
 
 	//insert user inf. to table t_user
 	public void insert(){
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rset = null;
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rset = null;
 
-        //接続文字列
-        String url = "jdbc:postgresql://localhost:5432/loginsystem";
-        String user = "postgres";
-        String password = "pwpsql";
+		//接続文字列
+		String url = "jdbc:postgresql://localhost:5432/loginsystem";
+		String user = "postgres";
+		String password = "pwpsql";
 
-        try{
-            //PostgreSQLへ接続
-            conn = DriverManager.getConnection(url, user, password);
+		try{
+			//PostgreSQLへ接続
+			conn = DriverManager.getConnection(url, user, password);
 
-            //自動コミットOFF
-            conn.setAutoCommit(false);
-            System.out.println("Opened database successfully");
+			//自動コミットOFF
+			conn.setAutoCommit(false);
+			System.out.println("Opened database successfully");
 
-            //INSERT文の実行
-            stmt = conn.createStatement();
-            String sql = "INSERT INTO T_USER (USER_ID, USER_PW, USER_NAME, DEPT_NO, RGST_DATE) VALUES("
-                  + id + ",'" + userPassword + "', '" + name + "', '" + deptNo + "', '" + rgstDate + "');";
-            stmt.executeUpdate(sql);
-            conn.commit();
-        }
-        catch (SQLException e){
-        	e.printStackTrace();
-        	System.exit(0);
-        }
-        finally {
-            try {
-                if(rset != null)rset.close();
-                if(stmt != null)stmt.close();
-                if(conn != null)conn.close();
-            }
-            catch (SQLException e){
-                e.printStackTrace();
-            }
+			//INSERT文の実行
+			stmt = conn.createStatement();
+			String sql = "INSERT INTO T_USER (USER_ID, USER_PW, USER_NAME, DEPT_NO, RGST_DATE) VALUES("
+					+ id + ",'" + userPassword + "', '" + name + "', '" + deptNo + "', '" + rgstDate + "');";
+			stmt.executeUpdate(sql);
+			conn.commit();
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+			System.exit(0);
+		}
+		finally {
+			try {
+				if(rset != null)rset.close();
+				if(stmt != null)stmt.close();
+				if(conn != null)conn.close();
+			}
+			catch (SQLException e){
+				e.printStackTrace();
+			}
 
-        }
-        System.out.println("Records created successfully");
-    }
+		}
+		System.out.println("Records created successfully");
+	}
 
 	//select all the inf. from t_user whose id = userId
 	public void selectInf(int userId) {
